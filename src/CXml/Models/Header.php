4eace7069598a8910fcbb0dc039237de58bf5a9a
Frank J. Dürring <frank.duerring@condero.com>
2023-08-03 11:11:59 +0200

    Update Header.php
    -add fromIdentity and fromDomain
    -add toIdentity and toDomain
    -add senderDomain

--- src/CXml/Models/Header.php
+++ src/CXml/Models/Header.php
@@ -6,25 +6,97 @@ use CXml\Models\Responses\ResponseInterface;
 
 class Header
 {
+    private $userAgent;
+    private $fromIdentity;
+    private $fromDomain;
+    private $toIdentity;
+    private $toDomain;
     private $senderIdentity;
+    private $senderDomain;
     private $senderSharedSecret;
 
+    public function getUserAgent()
+    {
+        return $this->userAgent;
+    }
+           
+    public function getFromIdentity()
+    {
+        return $this->fromIdentity;
+    }
+
+    public function getFromDomain()
+    {
+        return $this->fromDomain;
+    }
+    
+    public function getToIdentity()
+    {
+        return $this->toIdentity;
+    }
+
+    public function getToDomain()
+    {
+        return $this->toDomain;
+    }
+    
     public function getSenderIdentity()
     {
         return $this->senderIdentity;
     }
 
-    public function setSenderIdentity($senderIdentity): self
+    public function getSenderDomain()
     {
-        $this->senderIdentity = $senderIdentity;
-        return $this;
+        return $this->senderDomain;
     }
 
     public function getSenderSharedSecret()
     {
         return $this->senderSharedSecret;
     }
+    
+    public function setUserAgent($userAgent): self
+    {
+        $this->userAgent = $userAgent;
+        return $this;
+    }
 
+    public function setFromIdentity($fromIdentity): self
+    {
+        $this->fromIdentity = $fromIdentity;
+        return $this;
+    }
+
+    public function setFromDomain($fromDomain): self
+    {
+        $this->fromDomain = $fromDomain;
+        return $this;
+    }
+    
+    public function setToIdentity($toIdentity): self
+    {
+        $this->toIdentity = $toIdentity;
+        return $this;
+    }
+
+    public function setToDomain($toDomain): self
+    {
+        $this->toDomain = $toDomain;
+        return $this;
+    }
+    
+    public function setSenderIdentity($senderIdentity): self
+    {
+        $this->senderIdentity = $senderIdentity;
+        return $this;
+    }
+
+    public function setSenderDomain($senderDomain): self
+    {
+        $this->senderDomain = $senderDomain;
+        return $this;
+    }
+    
     public function setSenderSharedSecret($senderSharedSecret): self
     {
         $this->senderSharedSecret = $senderSharedSecret;
@@ -41,19 +113,18 @@ class Header
     {
         $headerNode = $parentNode->addChild('Header');
 
-        $this->addNode($headerNode, 'From', 'Unknown');
-        $this->addNode($headerNode, 'To', 'Unknown');
-        $this->addNode($headerNode, 'Sender', $this->getSenderIdentity() ?? 'Unknown')
-            ->addChild('UserAgent', 'Unknown');
+        $this->addNode($headerNode, 'From', $this->getFromIdentity() ?? 'Unknown', $this->getFromDomain() ?? '')
+            ->addChild('UserAgent', $this->setUserAgent() ?? 'Unknown');
+        $this->addNode($headerNode, 'To', $this->getToIdentity() ?? 'Unknown', $this->getToDomain() ?? '')
+            ->addChild('UserAgent', $this->setUserAgent() ?? 'Unknown');
+        $this->addNode($headerNode, 'Sender', $this->getSenderIdentity() ?? 'Unknown', $this->getSenderDomain() ?? '')
+            ->addChild('UserAgent', $this->setUserAgent() ?? 'Unknown');
     }
 
-    private function addNode(\SimpleXMLElement $parentNode, string $nodeName, string $identity) : \SimpleXMLElement
+    private function addNode(\SimpleXMLElement $parentNode, string $nodeName, string $identity, string $doamin) : \SimpleXMLElement
     {
         $node = $parentNode->addChild($nodeName);
-
-        $credentialNode = $node->addChild('Credential');
-        $credentialNode->addAttribute('domain', '');
-
+        $credentialNode->addAttribute('domain', $domain);
         $credentialNode->addChild('Identity', $identity);
 
         return $node;
